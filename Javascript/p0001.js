caafdde8a4fab05a3cd18c7c804f50f59d4ef647
RudyRed <willgross94@gmail.com>
2017-12-06 23:17:14 -0800

    Completed P0001

---
+++ Javascript/p0001.js
@@ -0,0 +1,34 @@
+// Multiples of 3 and 5
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5,
+// we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+function multiplesOf3And5Sum() {
+  let sum = 0;
+  let num = 3;
+
+  while (num < 1000) {
+    sum += num;
+    num += 3;
+  }
+
+  num = 5;
+  while (num < 1000) {
+    sum += num;
+    num += 5;
+  }
+
+  num = 15;
+  while (num < 1000) {
+    sum -= num;
+    num += 15;
+  }
+
+  return sum;
+}
+
+console.log(multiplesOf3And5Sum());
+
+// Answer: 233168
