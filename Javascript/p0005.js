f2240a279074bd2ae45628898fed28410b0cf5e6
RudyRed <willgross94@gmail.com>
2017-12-08 23:21:03 -0800

    Completed

---
+++ Javascript/p0005.js
@@ -0,0 +1,22 @@
+// Smallest multiple
+// Problem 5
+// 2520 is the smallest number that can be divided by each of the numbers from 1
+// to 10 without any remainder.
+//
+// What is the smallest positive number that is evenly divisible by all of the
+// numbers from 1 to 20?
+
+function smallestNumbDivisiableBy1toN(n) { // n must be mult of 10 for this func
+  for (let num = n; n === n; num += n) {
+    let countFlag = 0;
+    for (let divisor = 2; divisor < n; divisor++) {
+      if (num % divisor === 0) {
+        countFlag++;
+      }
+    }
+    if (countFlag === n - 2) return num;
+  }
+}
+console.log(smallestNumbDivisiableBy1toN(20))
+
+// Answer 232792560
